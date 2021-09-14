88fcef79cc8fb70d3f3adcfbe54d1a1ba2cbdc57
Chris <Kirch2@gmail.com>
2021-09-14 13:37:59 -0400

    added more react

--- restauwant-frontend/src/App.js
+++ restauwant-frontend/src/App.js
@@ -9,28 +9,36 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/jobs">Jobs</Link>
+          </li>
+          <li>
+            <Link to="/jobs/new">New Job</Link>
           </li>
           <li>
             <Link to="/dashboard">Dashboard</Link>
           </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
         </ul>
 
         <hr />
-
-        {/*
-        A <Switch> looks through all its children <Route>
-        elements and renders the first one whose path
-        matches the current URL. Use a <Switch> any time
-        you have multiple routes, but you want only one
-        of them to render at a time
-      */}
         <Switch>
           <Route exact path="/">
             Home
           </Route>
-          <Route path="/about">About</Route>
+          <Route exact path="/jobs">
+            Jobs
+          </Route>
+          <Route exact path="/jobs/new">
+            New Job
+          </Route>
           <Route path="/dashboard">Dashboard</Route>
+          <Route path="/login">Login</Route>
+          <Route path="/register">Register</Route>
         </Switch>
       </div>
     </Router>
