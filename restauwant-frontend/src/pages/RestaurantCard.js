aeb68810c600645db6cd87f39a5c5549cd696de2
Tarique Smith <tpsmith1992@gmail.com>
2021-09-16 11:03:33 -0400

    bare bones for posting a job

--- restauwant-frontend/src/pages/RestaurantCard.js
+++ restauwant-frontend/src/pages/RestaurantCard.js
@@ -1,6 +1,29 @@
 import React from "react";
 
 export function RestaurantCard({ restaurant }) {
+  const mapNameToJobTitle = {
+    1: "Chef",
+    2: "Manager",
+    3: "Bartender",
+    4: "Server",
+    5: "Busser",
+  };
+  
+  const mapBoroughToLabel = {
+    1: "Manhattan",
+    2: "Brooklyn",
+    3: "Queens",
+    4: "Staten Island",
+    5: "Bronx",
+  };
+  
+  const mapPriceToLabel = {
+    1: "$",
+    2: "$$",
+    3: "$$$",
+    4: "$$$$",
+    5: "$$$$$",
+  };
    
 return (
     <div className="card card-body">
@@ -10,9 +33,10 @@ return (
         style={{ height: "200px", width: "225px" }}
         alt="restaurant"
       />
+      <br></br>
       <h5 className="card-title">{restaurant.restaurant_type}</h5>
-      <p className="card-text">{restaurant.borough_location}</p>
-      <p className="card-text">{restaurant.price_range}</p>
+      <p className="card-text"><strong>Borough Location: </strong> {mapBoroughToLabel[restaurant.borough_location]}</p>
+      <p className="card-text">{mapPriceToLabel[restaurant.price_range]}</p>
       <p className="card-text">There are {restaurant.jobs.length} open positions for this restaurant</p>
     </div>
   );
